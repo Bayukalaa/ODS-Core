65c8278ce1dffa24c8ab12542596ea7df6bdd209
OnlyDarknes <ytbdrkns@gmail.com>
2025-06-20 01:34:27 +0300

    Initial commit

--- src/main/java/dev/onlydarknesss/ODSCore/Main.java
+++ src/main/java/dev/onlydarknesss/ODSCore/Main.java
@@ -1,31 +1,84 @@
 package dev.onlydarknesss.ODSCore;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public final class Main extends JavaPlugin {
+
     private static Main instance;
-    private static Boolean DEV_MODE = null;
-    private static String VERSION = null;
+    private static boolean DEV_MODE = false;
+    private static String VERSION = "pre-alpha";
+
+    private List<File> filesToCopy;
 
     @Override
-    public void onEnable(){
-        saveDefaultConfig();
+    public void onEnable() {
         instance = this;
-        File config = new File(String.valueOf(getConfig()));
+        saveDefaultConfig();
 
-        if(config.exists()){
-            DEV_MODE = getConfig().getBoolean("dev-mode", false);
-            VERSION = getConfig().getString("pre-alpha", "pre-alpha");
-        } else {
-            saveDefaultConfig();
-        }
+        FileConfiguration config = getConfig();
+        DEV_MODE = config.getBoolean("dev-mode", false);
+        VERSION = config.getString("pre-alpha", "pre-alpha");
 
-        if (DEV_MODE == true){
-            getLogger().info("Server is on maintenance, if you think there was an error, check config file or contact to developer");
+        filesToCopy = List.of(
+                new File(getDataFolder(), "config.yml")
+        );
+
+        if (DEV_MODE) {
+            getLogger().info("Server is on maintenance, if you think there was an error, check config file or contact the developer.");
+            maintenance(true);
         }
     }
 
-    public static Main getInstance(){return instance;}
+    private void maintenance(boolean devMode) {
+        File logFolder = new File(getDataFolder(), "Logs");
+
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss").format(new Date());
+        if (!logFolder.exists()) {
+            logFolder.mkdirs();
+        }
+
+        if (devMode) {
+            File timeStampedFolder = new File(logFolder, timeStamp);
+            if (!timeStampedFolder.exists()) {
+                timeStampedFolder.mkdirs();
+            }
+
+            for (File srcFile : filesToCopy) {
+                if (srcFile.exists()) {
+                    File destFile = new File(timeStampedFolder, srcFile.getName());
+                    try {
+                        copyFile(srcFile, destFile);
+                        getLogger().info("Copied: " + srcFile.getName());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    getLogger().warning("File not found: " + srcFile.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    private void copyFile(File source, File destination) throws IOException {
+        try (
+                InputStream in = new FileInputStream(source);
+                OutputStream out = new FileOutputStream(destination)
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
 }
